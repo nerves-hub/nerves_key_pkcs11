2e1049741c70235aeac2ee313f6ed65dbfb391d5
Frank Hunleth <fhunleth@troodon-software.com>
2020-05-11 12:56:56 -0400

    Bump deps localling and on CI

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule NervesKey.PKCS11.MixProject do
     [
       {:elixir_make, "~> 0.4", runtime: false},
       {:ex_doc, "~> 0.19", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0.0-rc.6", only: :dev, runtime: false}
+      {:dialyxir, "~> 1.0.0", only: :dev, runtime: false}
     ]
   end
 
