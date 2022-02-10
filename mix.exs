eaf9233a97e89fdb733e38f7921200cca06aa30d
Frank Hunleth <fhunleth@troodon-software.com>
2022-02-09 19:46:02 -0500

    Require Elixir 1.9 since that's what's tested

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule NervesKey.PKCS11.MixProject do
     [
       app: :nerves_key_pkcs11,
       version: @version,
-      elixir: "~> 1.7",
+      elixir: "~> 1.9",
       description: description(),
       package: package(),
       source_url: @source_url,
